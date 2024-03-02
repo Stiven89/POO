40c1aff6ac9e0e03d0084ea87315fc1d44792271
Stiven89 <116035503+Stiven89@users.noreply.github.com>
2024-03-02 11:40:40 -0500

    Update and rename Encapsulación.cpp to Encapsulación_Guia_01.cpp

--- Encapsulación_Guia_01.cpp
+++ Encapsulación_Guia_01.cpp
@@ -7,17 +7,17 @@ private:
     int codigo;
 
 public:
-    // Constructor que inicializa el c骴igo
+    // Constructor que inicializa el c贸digo
     CajaFuerte(int codigoInicial) {
         codigo = codigoInicial;
     }
 
-    // M閠odo para abrir la caja fuerte
+    // M茅todo para abrir la caja fuerte
     void abrir(int codigoIngresado) {
         if (codigoIngresado == codigo) {
             cout << "Caja fuerte abierta. Bien hecho." << endl;
         } else {
-            cout << "C骴igo incorrecto. Caja fuerte cerrada." << endl;
+            cout << "C贸digo incorrecto. Caja fuerte cerrada." << endl;
         }
     }
 };
@@ -26,10 +26,10 @@ int main() {
     // Crear una instancia de la caja fuerte
     CajaFuerte miCajaFuerte(1234);
 
-    // Intentar abrir la caja fuerte con un c骴igo incorrecto
+    // Intentar abrir la caja fuerte con un c贸digo incorrecto
     miCajaFuerte.abrir(4321);
 
-    // Intentar abrir la caja fuerte con el c骴igo correcto
+    // Intentar abrir la caja fuerte con el c贸digo correcto
     miCajaFuerte.abrir(1234);
 
     return 0;
