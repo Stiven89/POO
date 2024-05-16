075b252505206403a5f0f8c362a7074ae0316ef4
Stiven89 <116035503+Stiven89@users.noreply.github.com>
2024-05-16 15:57:51 -0500

    Create Throws_Guia_06.java

---
+++ Throws_Guia_06.java
@@ -0,0 +1,7 @@
+public class ThrowsExample {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Iniciando...");
+        Thread.sleep(2000); // Se pausa la ejecución durante 2 segundos
+        System.out.println("Finalizado");
+    }
+}
